3d7f9cad017de985bd0135627d4f28ad63d8e6f8
Bernat Borrás Paronella <bernatbor15@gmail.com>
2014-10-25 08:30:19 +0200

    ProfileLayout

---
+++ mobile/src/main/java/com/tempos21/cieguitos/ui/activity/ProfileActivity.java
@@ -0,0 +1,27 @@
+package com.tempos21.cieguitos.ui.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.tempos21.cieguitos.R;
+
+/**
+ * Created by Bernat on 25/10/2014.
+ */
+public class ProfileActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.profile_layout);
+
+		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+		setSupportActionBar(toolbar);
+
+		if (getSupportActionBar() != null)  {
+			getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		}
+	}
+}
--- mobile/src/main/java/com/tempos21/cieguitos/ui/fragment/DrawerFragment.java
+++ mobile/src/main/java/com/tempos21/cieguitos/ui/fragment/DrawerFragment.java
@@ -1,6 +1,7 @@
 package com.tempos21.cieguitos.ui.fragment;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -8,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.tempos21.cieguitos.R;
+import com.tempos21.cieguitos.ui.activity.ProfileActivity;
 
 /**
  * Created by Bernat on 25/10/2014.
@@ -32,7 +34,8 @@ public class DrawerFragment extends Fragment implements View.OnClickListener {
 	public void onClick(View v) {
 		switch (v.getId()) {
 			case R.id.miPerfil:
-
+				Intent intent = new Intent(getActivity(), ProfileActivity.class);
+				startActivity(intent);
 				break;
 		}
 	}
