be007331c10c373101649e1ce62eca2b23ea9494
Bernat Borrás Paronella <bernatbor15@gmail.com>
2014-10-25 03:39:15 +0200

    QR Activity

---
+++ wear/src/main/java/com/tempos21/cieguitos/ui/activity/LaunchActivity.java
@@ -0,0 +1,35 @@
+package com.tempos21.cieguitos.ui.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.tempos21.cieguitos.R;
+
+public class LaunchActivity extends Activity implements WatchViewStub.OnLayoutInflatedListener, View.OnClickListener {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_launch);
+
+		WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+		stub.setOnLayoutInflatedListener(this);
+	}
+
+	@Override
+	public void onLayoutInflated(WatchViewStub watchViewStub) {
+		watchViewStub.findViewById(R.id.qr).setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		Intent intent = new Intent(this, WearMainActivity.class);
+		startActivity(intent);
+		finish();
+	}
+}
