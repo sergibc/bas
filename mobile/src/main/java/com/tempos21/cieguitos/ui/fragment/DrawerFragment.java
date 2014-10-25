cf0394a7213e0351bb571ac37a3911e171175161
Bernat Borrás Paronella <bernatbor15@gmail.com>
2014-10-25 07:46:35 +0200

    Material profile drawer

--- mobile/src/main/java/com/tempos21/cieguitos/ui/fragment/DrawerFragment.java
+++ mobile/src/main/java/com/tempos21/cieguitos/ui/fragment/DrawerFragment.java
@@ -12,12 +12,28 @@ import com.tempos21.cieguitos.R;
 /**
  * Created by Bernat on 25/10/2014.
  */
-public class DrawerFragment extends Fragment {
+public class DrawerFragment extends Fragment implements View.OnClickListener {
 
 	@Nullable
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		super.onCreateView(inflater, container, savedInstanceState);
 		return inflater.inflate(R.layout.drawer_layout, container, false);
+	}
+
+	@Override
+	public void onViewCreated(View view, Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
+
+		view.findViewById(R.id.miPerfil).setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+			case R.id.miPerfil:
+
+				break;
+		}
 	}
 }
